27e15eaed1828126baeebeb26288170500913307
Crystal Barnhill <aeternitum@gmail.com>
2018-06-09 09:17:51 -0700

    Created list of cards in JS

--- js/app.js
+++ js/app.js
@@ -1,7 +1,19 @@
 /*
  * Create a list that holds all of your cards
  */
+let cards = ['fa-diamond', 'fa-diamond',
+            'fa-paper-plane-o', 'fa-paper-plane-o',
+            'fa-anchor', 'fa-anchor',
+            'fa-bolt', 'fa-bolt',
+            'fa-cube', 'fa-cube',
+            'fa-leaf', 'fa-leaf',
+            'fa-bicycle', 'fa-bicycle',
+            'fa-bomb', 'fa-bomb',
+            ];
 
+function generateCard(card) {
+    return `<li class="card" data-card="${card}"><i class="fa ${card}"></i></li>`;
+}
 
 /*
  * Display the cards on the page
@@ -36,6 +48,16 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+function initGame() {
+  let deck = document.querySelector('.deck');
+  let cardHTML = shuffle(cards).map(function(card) {
+    return generateCard(card);
+  });
+
+  deck.innerHTML = cardHTML.join('');
+}
+
+initGame();
 
 let allCards = document.querySelectorAll('.card');
 let openCards = [];
@@ -56,6 +78,6 @@ allCards.forEach(function(card) {
           openCards = [];
         }, 1000);
       }
-    }  
+    }
   });
 });
