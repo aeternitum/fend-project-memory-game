4e60893b57c8e4abe6462e0f96b43a7b5ec6a465
Crystal Barnhill <aeternitum@gmail.com>
2018-06-27 15:55:18 -0700

    Added the star rating function that works properly

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-//Make a list of all the cards
+//This is from scratch to see if I can get it. First going to have the list of cards followed by
+//declaring all the variables
 
 let cards = ['fa-diamond', 'fa-diamond',
             'fa-paper-plane-o', 'fa-paper-plane-o',
@@ -7,27 +8,18 @@ let cards = ['fa-diamond', 'fa-diamond',
             'fa-cube', 'fa-cube',
             'fa-leaf', 'fa-leaf',
             'fa-bicycle', 'fa-bicycle',
-            'fa-bomb', 'fa-bomb',
-            ];
+            'fa-bomb', 'fa-bomb'];
 
-let counter = document.querySelector('.moves');
-let stars = document.querySelectorAll('.fa-star');
+let moves = 0;
+let moveCounter = document.querySelector('.moves');
 let matchedCards = document.getElementsByClassName('match');
-let starsEarned = document.querySelectorAll('.stars li');
-//let closeIcon = document.querySelector('.close');
-//let modal = document.getElementById('congratsPopup');
+let stars = document.querySelectorAll('.fa-star');
 
-//Adds each cards HTML to the page
+
 function generateCard(card) {
-    return `<li class="card" data-card="${card}"><i class="fa ${card}"></i></li>`;
+  return `<li class="card" data-card="${card}"><i class="fa ${card}"></i></li>`;
 }
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -44,48 +36,24 @@ function shuffle(array) {
     return array;
 }
 
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  done- display the card's symbol (put this functionality in another function that you call from this one)
- *  done- add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  done- if the list already has another card, check to see if the two cards match
- *  done  + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *  done  + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-
 document.body.onload = initGame();
 
 function initGame() {
   let deck = document.querySelector('.deck');
   let cardHTML = shuffle(cards).map(function(card) {
-    return generateCard(card);
+    return generateCard(card)
   });
-
-  let moves = 0;
-  let moveCounter = document.querySelector('.moves');
   moves = 0;
   moveCounter.innerText = moves;
 
   deck.innerHTML = cardHTML.join('');
 }
 
-
 let allCards = document.querySelectorAll('.card');
 let openCards = [];
-let moves = 0;
-let moveCounter = document.querySelector('.moves');
 
 allCards.forEach(function(card) {
   card.addEventListener('click', function(e) {
-
-//Start timer for the game after first click
-    if (moves <= 1) {
-      startTimer();
-    }
-
     if (!card.classList.contains('open') && !card.classList.contains('show') && !card.classList.contains('match')) {
       openCards.push(card);
       card.classList.add('open', 'show');
@@ -101,40 +69,51 @@ allCards.forEach(function(card) {
           openCards[1].classList.add('show');
 
           openCards = [];
-       } else {
-       //If no match-hide
-           setTimeout(function() {
-             openCards.forEach(function(card) {
-               card.classList.remove('open', 'show');
-             });
+        } else {
+     //If no match-hide
+          setTimeout(function() {
+            openCards.forEach(function(card) {
+              card.classList.remove('open', 'show');
+            });
 
-             openCards = [];
-           }, 1000);
+            openCards = [];
+          }, 1000);
         }
 
-       moves += 1;
-       moveCounter.innerText = moves;
-     }
-   }
+      moveCount();
+      starRating();
+      }
+    }
   });
 });
 
-//Some code for the star rating that isn't working yet
-if (moves > 6 && moves < 10) {
-  for (let i = 0; i < 3; i++) {
-    if (i > 1) {
-      stars[i].style.visibility = 'collapse';
-    }
+//Move counter for the game
+function moveCount() {
+  moves++;
+  moveCounter.innerHTML = moves;
+  if (moves <= 1) {
+    second = 0;
+    minute = 0;
+    startTimer();
   }
-} else if (moves > 11) {
+}
+
+//Star rating for the game
+function starRating() {
+  if (moves > 8 && moves < 12) {
+    for (let i = 0; i < 3; i++) {
+      if (i > 1) {
+        stars[i].style.visibility = 'collapse';
+      }
+    }
+  } else if (moves > 13) {
     for (let i = 0; i < 3; i++) {
       if (i > 0) {
         stars[i].style.visibility = 'collapse';
       }
     }
+  }
 }
-
-
 
 //Timer for the game
 let second = 0;
@@ -154,9 +133,5 @@ function startTimer() {
  },1000);
 }
 
-//Modal function for when i get the modal created and working
-function modal() {
-
-}
-
 //Thanks to Mike Wales Memory Game video for helping me get started and working out some bugs.
+//Also thanks to Morgen Becker for helping me figure out how to get the star rating working and helping me with the functions.
